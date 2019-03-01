9a3aff0a52d867bc6b154c3c6e68e5cb2494392c
Brady Holt <brady.holt@gmail.com>
2019-03-01 17:29:03 -0600

    Move _redirects to back to root

---
+++ _site/newpost.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+#    This script creates a new blog post with metadata in ./_posts
+#    folder. Date will be generated according to the current time in
+#    the system.  Source: https://codereview.stackexchange.com/questions/158124/generate-a-jekyll-blog-post-template-using-shell
+#
+# Usage:
+#   newpost.sh "My Blog Post Title"
+#
+
+title=$1
+
+if [ -z "$title" ]; then
+    echo "usage: blog \"My New Blog\""
+    exit 1
+fi
+
+bloghome=$(cd "$(dirname "$0")"; pwd)
+url=$(echo "$title" | tr '[:upper:]' '[:lower:]' | tr ' ' '-')
+filename="$(date +"%Y-%m-%d")-$url.md"
+filepath=$bloghome/_posts/$filename
+
+# Create post file
+cat >> $filepath <<DELIM
+---
+layout: post
+title: "$title"
+permalink: /$url/
+---
+DELIM
+
+echo "Post file created: $filepath"
+
+# Open in editor
+code --goto $filepath:6 $bloghome
+
+echo "Once finished, save and then run `rake publish`"
